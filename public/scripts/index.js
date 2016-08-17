c41a4b57ce788604c2717a617c021599c369cf30
Mahesh <rmahesh@us.ibm.com>
2016-08-17 12:52:44 -0400

    Updated favorites fix

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -187,20 +187,19 @@ function addItem(item, isNew){
 	
 }
 
-
 function deleteItem(deleteBtnNode){
 	var row = deleteBtnNode.parentNode.parentNode;
     var attribId = row.getAttribute('data-id');
     if(attribId)
 	{
 		xhrDelete(REST_DATA + '?id=' + row.getAttribute('data-id'), function(){
-			row.remove();
+			row.parentNode.removeChild(row);
 		}, function(err){
 			console.error(err);
 		});
 	}
 	else if (attribId == null) {
-		row.remove();
+		row.parentNode.removeChild(row);
 	}
 }
 
