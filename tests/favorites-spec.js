7cdf10b9130e86696c7588bc0215faed8ae0bf0d
Gabriele de Capoa <gabriele.decapoa@gmail.com>
2018-08-07 21:15:48 +0200

    Refactor configuration file - fix test case (#2)

--- tests/favorites-spec.js
+++ tests/favorites-spec.js
@@ -4,12 +4,16 @@ const server = supertest.agent(require('../app.js'));
 
 describe('FAVORITES APIs', function () {
 
+    before('before', function (done) {
+        require('../db/db.js').initDBConnection().then(done).catch(done);
+    });
+
     it('GET FAVORITES', function (done) {
         server
             .get('/api/favorites')
             .set('Accept', 'application/json')
-            .expect('Content-Type', 'application/json')
-            .expect(201)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
             .end(function (err, res) {
                 if (err) {
                     return done(err);
