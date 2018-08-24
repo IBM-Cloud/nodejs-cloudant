394c687c1bfcb4df9cc7e5d8280b7b2f27f11fbb
damianovesperini <damiano.vesperini1@ibm.com>
2018-08-24 13:44:55 +0200

    add categories list

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -1,25 +1,20 @@
 var app = angular.module("myApp", []);
 app.controller("myController", function($scope) {
-    $scope.title = '';
-    $scope.description = '';
-    $scope.files = [
-        {id: 1, title: 'example', description: "this is a file"}
-    ];
-    $scope.edit = true;
-    $scope.error = false;
-    $scope.hideform = true;
-    $scope.editUser = function (id) {
-        $scope.hideform = false;
-        if (id == 'new') {
-            $scope.edit = true;
-            $scope.title = '';
-            $scope.description = '';
-        } else {
-            $scope.edit = false;
-            $scope.title = $scope.files[id - 1].title;
-            $scope.description = $scope.files[id - 1].description;
+
+        $scope.categories = ["Video", "BestPhoto", "Photo"];
+        $scope.addCategory = function () {
+            $scope.errortext = "";
+            if (!$scope.addMe) {return;}
+            if ($scope.categories.indexOf($scope.addMe) == -1) {
+                $scope.categories.push($scope.addMe);
+            } else {
+                $scope.errortext = "This category is already exists.";
+            }
         }
-    };
+        $scope.removeCategory = function (x) {
+            $scope.errortext = "";
+            $scope.categories.splice(x, 1);
+        }
     $scope.toggleAppInfo = function toggleAppInfo() {
         var node = document.getElementById('appinfo');
         node.style.display = node.style.display == 'none' ? '' : 'none';
